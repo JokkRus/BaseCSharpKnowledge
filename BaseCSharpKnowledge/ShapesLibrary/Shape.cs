6fa584f561f86e782d00f2562b7856fb7deac233
JokkRus <jokk@yandex.ru>
2019-10-25 20:06:19 +0300

    Add abstract class Shape

---
+++ BaseCSharpKnowledge/ShapesLibrary/Shape.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShapesLibrary
+{
+    public abstract class Shape : IDrawable
+    {
+        public event DrawHandler OnDraw;
+        Point _startPoint;
+        public Shape() { }
+        public Shape(Point point)
+        {
+            _startPoint = point;
+        }
+        public abstract void Draw();
+    }
+}
