6207a0cc08fffa6c799599126453eb2ef6a1a5a1
JokkRus <jokk@yandex.ru>
2019-10-25 19:17:13 +0300

    Create static class for drawing points in Console

---
+++ BaseCSharpKnowledge/ShapesLibrary/DrawInConsole.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace ShapesLibrary
+{
+    public static class DrawInConsole
+    {
+        public static void DrawPoint(object sender, DrawEventArgs args)
+        {
+            Console.WriteLine("I'm starting draw the point..");
+            Thread.Sleep(1000);
+            Console.SetCursorPosition(((Point)sender).X, ((Point)sender).Y);
+            Console.Write(".");
+            EndDraw();
+        }
+        private static void EndDraw()
+        {
+            Thread.Sleep(1000);
+            Console.Clear();
+        }
+    }
+}
